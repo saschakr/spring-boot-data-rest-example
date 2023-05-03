91794fe9794d5db672d68e09059cb978865fa746
Sascha Kreutz <skr@in-telegence.net>
2023-05-03 18:06:27 +0200

    fix issue by provided solutions

--- src/main/java/github/skreutz/SampleApplication.java
+++ src/main/java/github/skreutz/SampleApplication.java
@@ -1,5 +1,8 @@
 package github.skreutz;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +13,11 @@ public class SampleApplication {
         SpringApplication.run(SampleApplication.class, args);
     }
 
+    @Autowired
+    void configureObjectMapper(final ObjectMapper mapper) {
+        Hibernate6Module hibernate6Module = new Hibernate6Module();
+        hibernate6Module.disable(Hibernate6Module.Feature.USE_TRANSIENT_ANNOTATION);
+        hibernate6Module.enable(Hibernate6Module.Feature.FORCE_LAZY_LOADING);
+        mapper.registerModule(hibernate6Module);
+    }
 }
